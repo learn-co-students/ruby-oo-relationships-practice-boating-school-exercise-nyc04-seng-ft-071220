0b7e0675b28fa9701ada405eff42924cef3abca2
mirsafirst47 <habib.triande@gmail.com>
2020-08-06 17:55:16 -0400

    Boating school unfinished

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,47 @@
+require 'pry'
+
 class Instructor
 
+    attr_accessor :name 
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def passed_student(student, test_name)
+        passing = BoatingTest.all.select { |test| test.student.first_name == student.first_name && test.test_name == test_name}
+        if passing
+            passing.test_status = "passed"
+        else
+            BoatingTest.new(stutent, test_name, "passed", self)
+        end
+    end
+
+    def fail_student(student, test_name)
+        failing = BoatingTest.all.select { |test| test.student.first_name == student.first_name && test.test_name == test_name}
+        if failing
+            failing.test_status = "failed"
+        else
+            BoatingTest.new(stutent, test_name, "failed", self)
+        end
+    end
+
+    def all_students
+        Student.all.select { |student| student.instructor == self}
+    end
+
+
+
 end
+
+
+puff= Instructor.new("Ms.Puff")
+krabs= Instructor.new("Mr.Krabs")
+# binding.pry
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,36 @@
 class Student
 
+    attr_accessor :first_name
+    @@all = []
+    def initialize(first_name)
+        @first_name = first_name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def add_boating_test(test_name, test_status, instructor)
+        BoatingTest.new(self, test_name, test_status, instructor)
+    end
+
+    def all_instructors
+        Instructor.all.select { |instructor| instructor.student == self}
+    end
+
+    def self.find_student(first_name)
+        self.all.find { |student| student.first_name == first_name}
+    end
+
+    def grade_percentage
+
+    end
+
 end
+
+
+spongebob = Student.new("Spongebob")
+patrick= Student.new("Patrick")
+
+# binding.pry
