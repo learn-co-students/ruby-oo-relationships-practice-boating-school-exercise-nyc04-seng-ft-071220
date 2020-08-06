945807a00a3f8df81634a6ce90a5be34ae10ab37
sbalgobin94 <sbalgobin94@gmail.com>
2020-08-06 16:16:38 -0400

    first push

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,14 @@
 class Instructor
+    attr_accessor :name
 
+    @@all
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,19 @@
 class Student
+    attr_accessor :first_name
+
+    @@all = []
+
+    def initialize(first_name)
+        @first_name = first_name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def add_boating_test
+        BoatingTest.new
+    end
 
 end
