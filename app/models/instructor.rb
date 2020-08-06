9a8b28257e80aaa5d71fe40b2ceed94a16f61330
wlcreate <waverley.nyc@gmail.com>
2020-08-06 17:54:39 -0400

    commit 2

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -25,8 +25,34 @@ class Instructor
     end
 
     def pass_student(student_instance, test_name)
-        self.passed_students.find do |student|
-            student.name == student_instance.name && student.test_name
+        pending_student = self.all_students.find do |student|
+            
+            student.student.name == student_instance.name && student.test_name
         end
+        
+        if pending_student
+            pending_student.test_status = "passed"
+        else
+            pending_student = BoatingTest.new(student_instance, test_name, "passed", self)
+        end
+
+        pending_student
+        #binding.pry
+    end
+
+    def fail_student(student_instance, test_name)
+        pending_student = self.all_students.find do |student|
+            
+            student.student.name == student_instance.name && student.test_name
+        end
+        
+        if pending_student
+            pending_student.test_status = "failed"
+        else
+            pending_student = BoatingTest.new(student_instance, test_name, "failed", self)
+        end
+
+        pending_student
+        #binding.pry
     end
 end
