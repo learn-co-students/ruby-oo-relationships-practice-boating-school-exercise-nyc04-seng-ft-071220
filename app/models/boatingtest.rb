29b77454bf179f18cb20fe6e1cb2a55c6685d6f3
evilgeniusnyc <evilgeniusnyc@me.com>
2020-08-06 16:14:54 -0400

    boating test class complete

--- app/models/boatingtest.rb
+++ app/models/boatingtest.rb
@@ -1,3 +1,20 @@
 class BoatingTest
+    attr_accessor :student, :boating_test_name, :boating_test_status
+
+    @@all = []
+
+   def initialize
+    @student = student 
+    @boating_test_name = boating_test_name
+    @boating_test_status = boating_test_status
+    @instructor = instructor 
+    @@all << self
+   end
+   
+  def self.all 
+    @@all 
+  end  
 
 end
+
+
