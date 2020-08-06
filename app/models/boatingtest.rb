64656925fb1dea6c6bc92b23fa5b47115234cb7d
emjose <emjose@gmail.com>
2020-08-06 17:13:05 -0400

    first commit

--- app/models/boatingtest.rb
+++ app/models/boatingtest.rb
@@ -1,3 +1,42 @@
+# require 'bundler/setup'
+# Bundler.require
+# require_all 'app'
+# require_relative '../config/environment.rb'
+ require 'pry'
+# require 'require_all'
+# require_all 'models'
+
+# `BoatingTest` class:
+
+# * should initialize with Student (Object), a boating test name (String), 
+# a boating test status (String), and an Instructor (Object)
+
+# * `BoatingTest.all` returns an array of all boating tests
+
+# **NOTE**: Test status has three options only ("passed", "failed" and "pending")
+
 class BoatingTest
 
+    @@all = []
+    attr_accessor :student, :boating_test_name, :boating_test_status, :instructor
+
+
+    def initialize(student, boating_test_name, boating_test_status, instructor)
+        @student = student
+        @boating_test_name = boating_test_name
+        @boating_test_status = boating_test_status
+        @instructor = instructor
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
 end
+
+# test1 = BoatingTest.new("Bob", "SAT", "passed", "Sylwia")
+
+# binding.pry
+# 0
+# puts "hi"
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,36 @@
+# require_relative '../config/environment.rb'
+require 'pry'
+# require 'require_all'
+# require_all 'models'
+
+# `Student` class:
+
+# * should initialize with `first_name`
+
+# * `Student.all` should return all of the student instances
+
+# * `Student#add_boating_test` should initialize a new boating test with a Student (Object), 
+# a boating test name (String), a boating test status (String), and an Instructor (Object). 
+
+# * `Student#all_instructors` should return an array of instructors with whom this specific student took a boating test.
+
+# * `Student.find_student` will take in a first name and output the student (Object) with that name
+
+# * `Student#grade_percentage` should return the percentage of tests that the student has passed, 
+#a Float (so if a student has passed 3 / 9 tests that they've taken, this method should return the Float `33.33`)
+
 class Student
+
+    @@all = []
+    attr_accessor :name, :instructor, :boatingtest
+
+    def initialize(name_arg)
+        @name = name_arg
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
 
 end
