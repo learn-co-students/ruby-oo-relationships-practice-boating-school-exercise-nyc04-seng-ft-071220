b2ee06deaf52e7967a1493b66d23749b52ab6ad0
Chenyun Zhang <morningchenyun@gmail.com>
2020-08-06 16:37:16 -0400

    first try

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,20 @@
+require 'pry'
+require_relative "boatingtest.rb" 
+require_relative "instructor"
+
 class Instructor
 
+    attr_accessor 
+
+    @@all = []
+
+    def initialize(name)
+            @name = name 
+            @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+    
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,38 @@
-class Student
+require 'pry'
+require_relative "boatingtest.rb" 
+require_relative "instructor"
 
+class Student
+    
+    attr_accessor :first_name
+    
+    @@all = []
+
+    def initialize(first_name)
+        @first_name=first_name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def add_boating_test
+        # binding.pry
+        BoatingTest.new(self, "boat", "boat_test_status","instructor")
+        # binding.pry
+        # 0
+    end
+
+    def all_instructors
+        BoatingTest.all.select{|instructor|
+             instructor.student == self.first_name
+            #  binding.pry
+        }
+    end
 end
+
+
+Jason = Student.new("Jason")
+Jason.add_boating_test
+puts Jason.all_instructors
