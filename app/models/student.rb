ffbdb812bf63ce28f3badef2dd8bd234c7762044
wilsonng21 <wilsongch92@gmail.com>
2020-08-06 21:45:10 -0400

    all behaviors as expected

--- app/models/student.rb
+++ app/models/student.rb
@@ -19,6 +19,8 @@ class Student
 
     def add_boating_test(boating_test_name, boating_test_status, instructor) 
         test = BoatingTest.new(boating_test_name, boating_test_status, instructor)
+        #great difficulty here, we had trouble connecting the instance of student to each instance of BoatingTest
+        #test.student was returning *nil*, and then we finally realized all we had to do was assign student instance there.
         test.student = self
         test 
         # binding.pry
@@ -26,9 +28,31 @@ class Student
 
     def all_instructors
         result = BoatingTest.all.select{|test| test.student == self}
-        binding.pry
-
-        ### stuck here
+        #above selects all instances of BoatingTest that matches instance of student
+        instructor_array = result.map { |boating_test| boating_test.instructor.name}
+        #mutate and return what we need with map enum, which is all the instructors' name in string format and inside an array.
     end
+
+    def grade_percentage
+        student_results = BoatingTest.all.select{|test| test.student == self}
+        all_results_for_student = student_results.map { |boating_test| boating_test.boating_test_status}
+        # all_results_for_student.size is the total number we are dividing against the nums
+        # of "pass" test_status we have in the array.
+        # 1. gather all "pass" test_statuses 
+        # 3. gather total size of all test statuses
+        # 3. divide against all_test_results_forstudents.size
+        total_nums_of_grades = all_results_for_student.size
+        total_nums_of_passed_grades = all_results_for_student.count { |grade| grade == "passed" }
+        grade_percentage = total_nums_of_passed_grades.to_f / total_nums_of_grades.to_f
+        grade_percentage = grade_percentage * 100
+    end
+
+    def self.find_student(first_name)
+        self.all.find { |student| student.first_name == first_name}
+        # class method and the instructions said to return a student object and not string.
+        # output when calling this as Student.find_student(first_name) passes
+    end
+
+
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -11,14 +11,22 @@ end
 spongebob = Student.new("Spongebob")
 patrick = Student.new("Patrick")
 squidward = Student.new("Squidward")
+shirley = Student.new("Shirley")
 
-puff= Instructor.new("Ms.Puff")
-krabs= Instructor.new("Mr.Krabs")
+puff = Instructor.new("Ms.Puff")
+krabs = Instructor.new("Mr.Krabs")
+whale = Instructor.new("Mr.Whale")
 
 no_crashing = spongebob.add_boating_test("Don't Crash 101", "pending", puff)
 no_crashing2 = spongebob.add_boating_test("Don't Crash 102", "pending", krabs)
+no_crashing3 = shirley.add_boating_test("Don't Crash 102", "pending", krabs)
+no_crashing4 = patrick.add_boating_test("Don't Crash 101", "passed", krabs)
 power_steering_failure = patrick.add_boating_test("Power Steering 202", "failed", puff)
 power_steering_pass = patrick.add_boating_test("Power Steering 201", "passed", krabs)
+power_steering_pass2 = squidward.add_boating_test("Power Steering 201", "passed", krabs)
+power_steering_pass3 = squidward.add_boating_test("Power Steering 201", "passed", krabs)
+speedy_pass = shirley.add_boating_test("Speed Passing 101", "passed", whale)
+speedy_pass2 = squidward.add_boating_test("Speed Passing 101", "failed", whale)
 
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
