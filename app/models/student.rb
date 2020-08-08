992d525977f10566c1b5402e63811028bfe29e46
Dong-Yi-Xia <dong7777xia@gmail.com>
2020-08-08 12:30:45 -0400

    done

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,40 @@
 class Student
 
+    attr_accessor :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def all_test
+        BoatingTest.all.select do |test| 
+            test.student == self
+        end
+    end
+
+    def all_instructors
+        self.all_test.map(&:instructor)
+    end
+
+    def find_student(first_name)
+        @@all.find do |student|
+            student.name == first_name
+        end
+    end
+
+    def grade_percentage
+        num = self.all_test.count do |test|
+            test.status == "passed"
+        end
+        total = self.all_test.count 
+        num.to_f / total
+    end
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -8,6 +8,28 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+spongebob = Student.new("Spongebob")
+patrick= Student.new("Patrick")
+sandy= Student.new("Sandy")
+ 
+puff= Instructor.new("Ms.Puff")
+krabs= Instructor.new("Mr.Krabs")
+ 
+test1 = BoatingTest.new(spongebob, "drive101", "passed", puff)
+test2 = BoatingTest.new(patrick, "drive101", "failed", puff)
+test3 = BoatingTest.new(spongebob, "drive101", "pending", krabs)
+test4 = BoatingTest.new(spongebob, "drive102", "passed", krabs)
+test5 = BoatingTest.new(spongebob, "drive103", "passed", krabs)
+
+puff.pass_student(patrick, "drive101")
+spongebob.find_student("Spongebob")
+spongebob.grade_percentage
+
+# no_crashing = spongebob.add_boating_test("Don't Crash 101", "pending", puff)
+# power_steering_failure = patrick.add_boating_test("Power Steering 202", "failed", puff)
+# power_steering_pass = patrick.add_boating_test("Power Steering 201", "passed", krabs)
+
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
 
