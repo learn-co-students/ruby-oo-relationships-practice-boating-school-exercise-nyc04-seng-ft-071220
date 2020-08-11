25dcb7981053b6ad870fa9496b1635d15fdcbd1b
wlcreate <waverley.nyc@gmail.com>
2020-08-11 09:30:41 -0400

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -17,4 +17,17 @@ class Student
     def add_boating_test(test_name, test_status, instructor)
         BoatingTest.new(self, test_name, test_status, instructor)
     end
+
+    def all_instructors
+        BoatingTest.all.select do |boating_test|
+            boating_test.student == self
+        end
+    end
+
+    def self.find_student(name)
+        self.all.find do |student|
+            student.name == name
+        end
+    end
+
 end
